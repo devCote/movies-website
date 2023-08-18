6ca78f3aa78afaee8e802d1064c2f3309cbe49d5
devcote <bahanenko@gmail.com>
2023-08-18 22:05:03 +0300

    added hotrefresh

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,40 +1,40 @@
-import path from 'path'
-import webpack from 'webpack'
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOprions } from "./types/config";
+import CopyWebpackPlugin from "copy-webpack-plugin";
 
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import { BuildOprions } from './types/config'
-import CopyWebpackPlugin from 'copy-webpack-plugin';
+export function buildPlugins({
+    paths,
+    isDev,
+}: BuildOprions): webpack.WebpackPluginInstance[] {
+    const progressPlugin = new webpack.ProgressPlugin();
+    const htmlPlugin = new HtmlWebpackPlugin({
+        template: paths.html,
+        favicon: paths.favicon,
+    });
+    const miniCssPlugin = new MiniCssExtractPlugin({
+        filename: "css/[name].[contenthash:8].css",
+        chunkFilename: "css/[name].[contenthash:8].css",
+    });
+    const definePlugin = new webpack.DefinePlugin({
+        __IS_DEV__: JSON.stringify(isDev),
+    });
+    const copyPlugin = new CopyWebpackPlugin({
+        patterns: [{ from: "public/locales/", to: "locales" }],
+    });
+    const hotReloadPlugin = new webpack.HotModuleReplacementPlugin();
 
-export function buildPlugins({paths, isDev}: BuildOprions): webpack.WebpackPluginInstance[] {
+    const plugins = [
+        progressPlugin,
+        htmlPlugin,
+        miniCssPlugin,
+        definePlugin,
+        copyPlugin,
+    ];
 
-  const progressPlugin = new webpack.ProgressPlugin()
+    if (isDev) plugins.push(hotReloadPlugin);
 
-  const htmlPlugin = new HtmlWebpackPlugin({
-      template: paths.html,
-      favicon: paths.favicon,
-    })
-
-  const miniCssPlugin = new MiniCssExtractPlugin({
-    filename: 'css/[name].[contenthash:8].css',
-    chunkFilename: 'css/[name].[contenthash:8].css'
-  })
-
-  const definePlugin = new webpack.DefinePlugin({
-    __IS_DEV__: JSON.stringify(isDev)
-  })
-
-  const copyPlugin = new CopyWebpackPlugin({
-    patterns: [
-        { from: "public/locales/", to: "locales" }
-    ]})
-
-  return [
-    progressPlugin,
-    htmlPlugin,
-    miniCssPlugin,
-    definePlugin,
-    copyPlugin
-  ]
-  
+    return plugins;
 }
