8de3282f35859f13667c7efeaa7ad5161d1bb36f
devcote <bahanenko@gmail.com>
2023-08-18 19:43:39 +0300

    tested i38n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,17 +2,37 @@ import { AppRouter, useTheme } from "app/providers";
 import { classNames } from "shared";
 import "./styles/index.scss";
 import { Navbar, Sidebar } from "widgets";
+import { Suspense } from "react";
+import { useTranslation } from "react-i18next";
+
+const Component = () => {
+    const { t, i18n } = useTranslation();
+
+    const toggleLang = () => {
+        i18n.changeLanguage(i18n.language === "en" ? "ru" : "en");
+    };
+
+    return (
+        <div>
+            <button onClick={toggleLang}>{t("translate")}</button>
+            <div>{t("bitch")}</div>
+        </div>
+    );
+};
 
 export const App = () => {
-    const { theme, toggleTheme } = useTheme();
+    const { theme } = useTheme();
 
     return (
         <div className={classNames("app", {}, [theme])}>
-            <Navbar />
-            <div className="content-page">
-                <Sidebar />
-            <AppRouter />
-            </div>
+            <Suspense fallback="LOADING">
+                <Navbar />
+                <Component />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
