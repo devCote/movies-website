03041a9d9e3d59cb20469f70198d780737602d63
devcote <bahanenko@gmail.com>
2023-08-20 03:07:25 +0300

    created app router loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,14 @@ import { classNames } from 'shared'
 import './styles/index.scss'
 import { Navbar, Sidebar } from 'widgets'
 import { Suspense } from 'react'
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader'
 
 export const App = () => {
     const { theme } = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback="LOADING">
+            <Suspense fallback={<PageLoader />}>
                 <Navbar />
 
                 <div className="content_page">
