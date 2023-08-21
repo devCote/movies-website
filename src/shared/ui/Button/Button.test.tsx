1a263a07adb45b4bdf4e93a6e80df04e669614dd
devcote <bahanenko@gmail.com>
2023-08-21 22:16:20 +0300

    button component tests

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,9 +1,13 @@
 import { render, screen } from '@testing-library/react'
-import { Button } from 'shared/ui/Button/Button'
+import { Button, ThemeButton } from 'shared/ui/Button/Button'
 
 describe('Button Tests', () => {
     test('display button in DOM', () => {
         render(<Button>test</Button>)
-        expect(screen.getByText('test')).toBeVisible()
+        expect(screen.getByText('test')).toBeInTheDocument()
+    })
+    test('button has className', () => {
+        render(<Button theme={ThemeButton.CLEAR}>test</Button>)
+        expect(screen.getByText('test')).toHaveClass('clear')
     })
 })
